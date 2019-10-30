21ce9122d15d4221e4b8a645143f3a4b9edaa36c
Can LO <43052031+canlo33@users.noreply.github.com>
2019-10-30 15:28:28 -0400

    Animation Setup + Player Controller Script
    1) Finished setting up most of the animations.
    
    2) Started working on Player Controller Script which will control player's movements/animations/attacks.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    private bool canCombo;
+    private int clickCounter;
+ 
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        clickCounter = 0;
+        canCombo = false;
+        
+    }
+
+    void Update()
+    {
+        Attack();
+        Run();
+    }
+
+    void ResetAnimationParameters()
+    {
+        animator.SetBool("jump", false);
+        animator.SetBool("idle", false);        
+        animator.SetBool("walk", false);
+        animator.SetBool("attack", false);
+        animator.SetBool("attack1", false);
+        animator.SetBool("attack2", false);
+    }
+
+    void Attack()
+    {
+        if (Input.GetMouseButtonDown(0) && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            ResetAnimationParameters();
+            animator.SetBool("attack1", true);
+            
+        }
+
+        else if (Input.GetMouseButtonDown(0))
+        {
+            ResetAnimationParameters();
+            animator.SetBool("attack", true);
+            
+        }
+
+        
+    }
+
+    void Run()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            ResetAnimationParameters();
+            animator.Play("Run");   
+            
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            ResetAnimationParameters();
+            animator.Play("Run");
+        }
+        else
+        {
+            ResetAnimationParameters();
+            animator.Play("Idle");
+        }
+
+    }
+
+    public void Idle()
+    {
+        ResetAnimationParameters();
+        animator.SetBool("idle", true);
+    }
+      
+
+
+}
