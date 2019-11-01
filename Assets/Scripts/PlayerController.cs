5db320aa2449e83f581b326e512e0556cf7bfe7c
Can LO <43052031+canlo33@users.noreply.github.com>
2019-10-31 22:23:16 -0400

    PlayerController Script Implementation
    1) Player is now able to run/jump/attack/combo attack.
    
    2) Run/Jump/Attack animations are completed. Combo attack animation needs to be revised.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,81 +6,114 @@ public class PlayerController : MonoBehaviour
 {
     private Animator animator;
     private bool canCombo;
-    private int clickCounter;
- 
+    public float comboTimer;
+    public int clickCounter;
+    private float horizontalMove;
+    private float verticalMove;
+    public float moveSpeed;
+    public float jumpSpeed;
+    private Rigidbody2D rigidbody;
+    private bool isJumping;
+
 
     void Start()
     {
         animator = GetComponent<Animator>();
         clickCounter = 0;
+        comboTimer = -1f;
         canCombo = false;
+        isJumping = false;
+        rigidbody = GetComponent<Rigidbody2D>();
         
     }
 
     void Update()
     {
-        Attack();
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        animator.SetFloat("run", Mathf.Abs(horizontalMove));
+
+        comboTimer -= Time.deltaTime;
+        if(comboTimer <= 0f)
+        {
+            clickCounter = 0;
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
         Run();
+        Attack();
+        Jump();
     }
 
     void ResetAnimationParameters()
     {
-        animator.SetBool("jump", false);
-        animator.SetBool("idle", false);        
-        animator.SetBool("walk", false);
-        animator.SetBool("attack", false);
-        animator.SetBool("attack1", false);
-        animator.SetBool("attack2", false);
+        animator.SetBool("jump", false);       
+        animator.SetBool("run", false);
+        animator.SetBool("combo", false);
+        animator.SetBool("slash", false);
+        animator.SetBool("chain", false);
     }
 
     void Attack()
     {
-        if (Input.GetMouseButtonDown(0) && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        if (Input.GetMouseButtonUp(0) && !isJumping && clickCounter == 0)
         {
-            ResetAnimationParameters();
-            animator.SetBool("attack1", true);
+            animator.SetBool("slash", true);
+            clickCounter++;
+            comboTimer = .90f;
+        }
+        else if (Input.GetMouseButtonDown(0) && !isJumping && clickCounter == 1 && comboTimer >= 0f && animator.GetFloat("run") < 0.01)
+        {
+            clickCounter--;
+            animator.SetBool("slash", false);
+            animator.SetBool("chain", true);
+
+        }
+    } 
+
+    private void Run()
+    {
+        if(horizontalMove > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
             
         }
-
-        else if (Input.GetMouseButtonDown(0))
+        else if (horizontalMove < 0)
         {
-            ResetAnimationParameters();
-            animator.SetBool("attack", true);
-            
-        }
+            GetComponent<SpriteRenderer>().flipX = true;
 
+        }
+        transform.Translate(horizontalMove * Time.fixedDeltaTime, 0f, 0f);
+
+    }
+
+    private void Jump()
+    {
+        if(Input.GetButton("Jump") && !isJumping)
+        {
+            rigidbody.velocity = Vector2.up * jumpSpeed;
+            animator.SetBool("jump", true);
+            isJumping = true;
+        }
+        if(rigidbody.velocity.y <= 0)
+        {
+            animator.SetBool("jump", false);
+            
+        }       
         
     }
 
-    void Run()
-    {
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
-            ResetAnimationParameters();
-            animator.Play("Run");   
-            
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
-            ResetAnimationParameters();
-            animator.Play("Run");
-        }
-        else
-        {
-            ResetAnimationParameters();
-            animator.Play("Idle");
-        }
 
-    }
-
-    public void Idle()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         ResetAnimationParameters();
-        animator.SetBool("idle", true);
+        isJumping = false;
     }
-      
+
+
+
 
 
 }
